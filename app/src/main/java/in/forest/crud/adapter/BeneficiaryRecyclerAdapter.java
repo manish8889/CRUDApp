f6a0eb2a86fc1445857755261365e3cf171c1085
Manish Kumar <m.manishec@gamil.com>
2024-04-10 14:13:06 +0530

    android app

---
+++ app/src/main/java/in/forest/crud/adapter/BeneficiaryRecyclerAdapter.java
@@ -0,0 +1,90 @@
+package in.forest.crud.adapter;
+
+import android.content.Context;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+
+
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import in.forest.crud.R;
+import in.forest.crud.model.Beneficiary;
+
+/**
+ * Created by delaroy on 5/10/17.
+ */
+public class BeneficiaryRecyclerAdapter extends RecyclerView.Adapter<BeneficiaryRecyclerAdapter.BeneficiaryViewHolder>  {
+
+    private ArrayList<Beneficiary> listBeneficiary;
+    public ImageView overflow;
+    private Context mContext;
+    private ArrayList<Beneficiary> mFilteredList;
+
+
+    public BeneficiaryRecyclerAdapter(ArrayList<Beneficiary> listBeneficiary, Context mContext) {
+        this.listBeneficiary = listBeneficiary;
+        this.mContext = mContext;
+        this.mFilteredList = listBeneficiary;
+
+
+    }
+
+    public class BeneficiaryViewHolder extends RecyclerView.ViewHolder {
+
+        public AppCompatTextView textViewName;
+        public AppCompatTextView textViewEmail;
+        public AppCompatTextView textViewAddress;
+        public AppCompatTextView textViewCountry;
+        public  ImageView overflow;
+
+        public BeneficiaryViewHolder(View view) {
+            super(view);
+            textViewName = (AppCompatTextView) view.findViewById(R.id.textViewName);
+            textViewEmail = (AppCompatTextView) view.findViewById(R.id.textViewEmail);
+            textViewAddress = (AppCompatTextView) view.findViewById(R.id.textViewAddress);
+            textViewCountry = (AppCompatTextView) view.findViewById(R.id.textViewCountry);
+
+        }
+
+
+    }
+
+
+
+
+    @Override
+    public BeneficiaryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // inflating recycler item view
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_beneficiary_recycler, parent, false);
+
+        return new BeneficiaryViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final BeneficiaryViewHolder holder, int position) {
+        holder.textViewName.setText(listBeneficiary.get(position).getName());
+        holder.textViewEmail.setText(listBeneficiary.get(position).getEmail());
+        holder.textViewAddress.setText(listBeneficiary.get(position).getAddress());
+        holder.textViewCountry.setText(listBeneficiary.get(position).getCountry());
+
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mFilteredList.size();
+    }
+
+
+
+}
